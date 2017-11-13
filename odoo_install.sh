0d380bd0943299c28ce6be8161bfeeefcc12e525
Dinh Duong Ha <dinhduongha@gmail.com>
2017-11-13 23:16:19 +0700

    Update link.

--- odoo_install.sh
+++ odoo_install.sh
@@ -44,8 +44,8 @@ UBUNTU_FIX_LOCAL="False"
 ## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
 ## in order to have correct version of wkhtmltox installed, for a danger note refer to 
 ## https://www.odoo.com/documentation/8.0/setup/install.html#deb ):
-WKHTMLTOX_X64=http://download.gna.org/wkhtmltopdf/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
-WKHTMLTOX_X32=http://download.gna.org/wkhtmltopdf/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb
+WKHTMLTOX_X64=https://downloads.wkhtmltopdf.org/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
+WKHTMLTOX_X32=https://downloads.wkhtmltopdf.org/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb
 
 #--------------------------------------------------
 # Update Server
@@ -86,7 +86,7 @@ echo -e "\n---- Install python packages ----"
 apt-get install python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil -y python-suds
 	
 echo -e "\n---- Install python libraries ----"
-pip install gdata psycogreen ofxparse XlsxWriter oauthlib pysftp
+pip install gdata psycogreen ofxparse XlsxWriter oauthlib pysftp xlrd
 
 echo -e "\n--- Install other required packages"
 apt-get install node-clean-css -y
